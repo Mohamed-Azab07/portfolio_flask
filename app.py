82c080989806faa11345a188cbdc40ae11dc26dc
Mohamed Azab <cysa.mohamedazab@gmail.com>
2023-02-16 12:22:56 +0300

    Last Edit in Python, HTML and CSS

--- app.py
+++ app.py
@@ -4,51 +4,48 @@ import smtplib
 from email.message import EmailMessage
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+	return render_template("index.html")
 
 @app.route("/sendemail/", methods=['POST'])
 def sendemail():
-    if request.method == "POST":
-        name = request.form['name']
-        subject = request.form['Subject']
-        email = request.form['_replyto']
-        message = request.form['message']
+	if request.method == "POST":
+		name = request.form['name']
+		subject = request.form['Subject']
+		email = request.form['_replyto']
+		message = request.form['message']
 
-        # Set your credentials
-        yourEmail = ""
-        yourPassword = "########"
+		# Set your credentials
+		yourEmail = "test_mail2024@mail.ru"
+		yourPassword = "AMUxAutjtYf1kjmbAhWZ"
 
-        # Logging in to our email account
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.login(yourEmail, yourPassword)
+		# Logging in to our email account
+		server = smtplib.SMTP('smtp.mail.ru', 587)
+		server.ehlo()
+		server.starttls()
+		server.login(yourEmail, yourPassword)
 
-        # Sender's and Receiver's email address
-        msg = EmailMessage()
-        msg.set_content("First Name : "+str(name)
-                        + "\nEmail : "+str(email)
-                        + "\nSubject : "+str(subject)
-                                        + "\nMessage : "+str(message))
-        msg['To'] = email
-        msg['From'] = yourEmail
-        msg['Subject'] = subject
+		# Sender's and Receiver's email address
+		msg = EmailMessage()
+		msg.set_content("First Name : "+str(name)
+						+"\nEmail : "+str(email)
+						+"\nSubject : "+str(subject)
+						+"\nMessage : "+str(message))
+		msg['To'] = email
+		msg['From'] = yourEmail
+		msg['Subject'] = subject
 
-        # Send the message via our own SMTP server.
-        try:
-            # sending an email
-            server.send_message(msg)
-            print("Send")
-        except:
-            print("Fail to Send")
-            pass
-
-    return redirect('/')
-
+		# Send the message via our own SMTP server.
+		try:
+			# sending an email
+			server.send_message(msg)
+			print("Send")
+		except:
+			print("Fail to Send")
+			pass
+			
+	return redirect('/')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
